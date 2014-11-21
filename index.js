57d3b752a973a929c333125e7c919790e88fdbdd
Dave Wasmer <davewasmer@gmail.com>
2014-11-21 18:32:53 -0500

    initial pass

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 'use strict';
+var path = require('path');
+var fontcustom = require('broccoli-fontcustom');
 
 module.exports = {
-  name: 'ember-cli-fontcustom'
+  name: 'ember-cli-fontcustom',  
+  included: function(app) {
+    if (app.options.icons) {
+      this.svgSources = app.options.icons.svgSources;
+      this.iconCSSTemplate = app.options.icons.cssTemplate;
+      this.iconOutput = app.options.icons.dest;
+    }
+    // Store appRoot so fontcustom can find the icon sources
+    this.appRoot = app.project.root;
+    
+    this._super.included.apply(this, arguments);
+  },
+
+  // Build font files
+  // TODO this isn't ideal - icons.css should be include-able in my SCSS/LESS build
+  treeForPublic: function(tree) {
+    var iconDestPath = this.iconDest ? 
+      path.join('assets', this.iconDest) : 
+      'assets';
+    var svgSources = this.svgSources || path.join('app', 'styles', 'icons');
+    if (typeof svgSources === 'string') {
+      svgSources = path.join(this.appRoot, svgSources);
+    }
+    var templatePath = this.iconCSSTemplate ? 
+      path.join(this.appRoot, this.iconCSSTemplate) : 
+      path.join(__dirname, 'addon', 'icons.css');
+
+    var fontTree = fontcustom(svgSources, {
+      output: iconDestPath,
+      templates: [ templatePath ]
+    });
+    return this.mergeTrees([ tree, fontTree ]);
+  }
 };
