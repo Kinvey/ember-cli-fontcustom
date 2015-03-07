eb9e7ba9f3c0661e3aea232a3be4c892d69f47fe
Dave Wasmer <davewasmer@gmail.com>
2015-03-07 00:40:00 -0500

    add contentFor to automate including icons.css

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 var fontcustom = require('broccoli-fontcustom');
 
 module.exports = {
-  name: 'ember-cli-fontcustom',  
+  name: 'ember-cli-fontcustom',
   included: function(app) {
     if (app.options.icons) {
       this.svgSources = app.options.icons.svgSources;
@@ -12,22 +12,28 @@ module.exports = {
     }
     // Store appRoot so fontcustom can find the icon sources
     this.appRoot = app.project.root;
-    
+
     this._super.included.apply(this, arguments);
+  },
+
+  contentFor: function(type) {
+    if (type === 'head') {
+      return '<link rel="stylesheet" href="assets/icons.css">';
+    }
   },
 
   // Build font files
   // TODO this isn't ideal - icons.css should be include-able in my SCSS/LESS build
   treeForPublic: function(tree) {
-    var iconDestPath = this.iconDest ? 
-      path.join('assets', this.iconDest) : 
+    var iconDestPath = this.iconDest ?
+      path.join('assets', this.iconDest) :
       'assets';
     var svgSources = this.svgSources || path.join('app', 'styles', 'icons');
     if (typeof svgSources === 'string') {
       svgSources = path.join(this.appRoot, svgSources);
     }
-    var templatePath = this.iconCSSTemplate ? 
-      path.join(this.appRoot, this.iconCSSTemplate) : 
+    var templatePath = this.iconCSSTemplate ?
+      path.join(this.appRoot, this.iconCSSTemplate) :
       path.join(__dirname, 'addon', 'icons.css');
 
     var fontTree = fontcustom(svgSources, {
